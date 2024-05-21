39f8225d061d16fb21254e06044cf765626e2484
Evellyn Karla <evellynkarla1418@gmail.com>
2024-05-21 16:35:00 -0300

    erro

--- poo/main.py
+++ poo/main.py
@@ -1,8 +1,5 @@
 from classes import *
 
-
-<<<<<<< HEAD
-
 # ================= Testes classe pessoa ======================
 # p1 = Pessoa("João",65, 23)
 # p2 = Pessoa("Maria",68, 24)
@@ -14,28 +11,25 @@ from classes import *
 #
 # p1.comer("coxinha")
 # p1.comer()
-=======
 # print(f'O {p1.nome} tem {p1.idade} anos e pesa {p1.peso}Kgs')
 # print(f'A {p2.nome} tem {p2.idade} anos e pesa {p2.peso}Kgs')
 
-print()
+# print()
 
-p1.comer("coxinha")
-p1.falar("oi", p2.nome)
-p1.pararFalar()
-p1.dormir()
-p1.pararComer()
-p1.dormir()
-print()
-p2.comer("pizza")
-p2.pararComer()
-p2.falar("oi", p1.nome)
-p2.dormir()
-p2.pararFalar()
-p2.dormir()
+# p1.comer("coxinha")
+# p1.falar("oi", p2.nome)
+# p1.pararFalar()
+# p1.dormir()
+# p1.pararComer()
+# p1.dormir()
+# print()
+# p2.comer("pizza")
+# p2.pararComer()
+# p2.falar("oi", p1.nome)
+# p2.dormir()
+# p2.pararFalar()
+# p2.dormir()
 
->>>>>>> bca368b69dd7a4905d633e76033f22e6837894c2
-#
 # print()
 #
 # p1.dormir()
@@ -79,5 +73,3 @@ gato.miar()
 
 cachorro = Cachorro("Meg", "caramelada lightskin")
 cachorro.latir()
-
-
